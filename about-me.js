ed025091b7a1d7456f4bd49efc814387dff38409
Amna  mansoor <amna.mansoor@Amnas-MacBook-Pro.local>
2016-03-16 11:00:51 -0700

    update about-me.js

---
+++ about-me.js
@@ -0,0 +1,51 @@
+console.log('About-me page for javascript');
+alert ('Welcome to Amna\'s page!!!');
+
+console.log('Yes/no');
+var amnasAge = prompt('Do you think I am in my 20s ?');
+if (amnasAge == 'yes') {
+  alert ('Your guess is right!');
+}
+
+else {
+  alert('Your guess is wrong!');
+}
+
+console.log('yes/no');
+var answer = prompt ('Do you think I have kids?');
+if (answer == 'yes') {
+  alert ('Your guess is right!');
+}
+
+else {
+  alert('Your guess is wrong!');
+}
+
+console.log('yes/no');
+var answer = prompt ('Do you think I was born in U.S?');
+if (answer == 'yes') {
+  alert ('Your guess is wrong!');
+}
+else {
+  alert ('Your guess is right!');
+}
+
+console.log('yes/no');
+var answer = prompt('Do you think I love music?');
+if (answer == 'yes'){
+  alert ('Your guess is right!');
+}
+else {
+  alert ('Your guess is wrong!');
+}
+
+console.log('yes/no');
+var answer =  prompt ('Do you think I currently live in Seattle?');
+if (answer == 'yes') {
+  alert ('Your guess is wrong!');
+}
+    else {
+  alert ('Your guess is right!');
+}
+console.log('thanks');
+alert ('Thanks for visiting my page. Have a good day!');
